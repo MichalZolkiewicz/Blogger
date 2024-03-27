ed5c0177defaaeac63ae1d4bd3b4633b28b9a113
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-28 00:57:35 +0100

    dependency injection classes for each project

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddScoped<IPostService, PostService>();
+
+        return services;
+    }
+}
