e101792e34c59a982265efa32ed78f461dcbed43
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-30 00:42:28 +0100

    mapping profile change

--- Application/Dto/PostDto.cs
+++ Application/Dto/PostDto.cs
@@ -9,9 +9,11 @@ public class PostDto : IMap
     public int Id { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
+    public DateTime CreationDate { get; set; }
 
     public void Mapping(Profile profile)
     {
-        profile.CreateMap<Post, PostDto>();
+        profile.CreateMap<Post, PostDto>()
+            .ForMember(x => x.CreationDate, y => y.MapFrom(z => z.Created));
     }
 }
