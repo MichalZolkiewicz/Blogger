10cf5459116c9e468e116ff0de1100d63b2fc26d
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-03-30 00:42:51 +0100

    OData implemented and registered

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,33 +1,53 @@
 using Application;
+using Application.Dto;
 using Cosmonaut;
 using Cosmonaut.Extensions.Microsoft.DependencyInjection;
 using Domain.Entities.Cosmos;
 using Infrastructure;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using Microsoft.OpenApi.Models;
+using OData.Swagger.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.EnableAnnotations();
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
+    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
 });
+
 
 builder.Services.AddInfrastructure();
 builder.Services.AddApplication();
 builder.Services.AddApiVersioning(x =>
 {
-    x.DefaultApiVersion = new ApiVersion(2, 0);
+    x.DefaultApiVersion = new ApiVersion(1, 0);
     x.AssumeDefaultVersionWhenUnspecified = true;
     x.ReportApiVersions = true;
 });
+
+builder.Services.AddControllers().AddOData(
+    options => options.Select().Filter().OrderBy().SetMaxTop(10).AddRouteComponents("odata", GetEdmModel()));
+
+builder.Services.AddOdataSwaggerSupport();
+
+
 builder.Services.AddDbContext<BloggerContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("BloggerCS")));
 
@@ -37,9 +57,7 @@ var cosmosStoreSettings = new CosmosStoreSettings(
     builder.Configuration["CosmosSettings:AccountKey"],
     new ConnectionPolicy { ConnectionMode = ConnectionMode.Direct, ConnectionProtocol = Protocol.Tcp }
     );
-
 builder.Services.AddCosmosStore<CosmosPost>(cosmosStoreSettings);
-
 var app = builder.Build();  
 
 // Configure the HTTP request pipeline.
@@ -49,10 +67,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
+
+static IEdmModel GetEdmModel()
+{
+    var builder = new ODataConventionModelBuilder();
+    builder.EntitySet<PostDto>("Posts");
+    return builder.GetEdmModel();
+}
 
 app.Run();
