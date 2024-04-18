f5649af1f85b404cf64ab73264941355d7ed66c6
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-04-18 19:46:15 +0200

    endpoint for adding superuser created

--- WebAPI/Controllers/V1/IdentityController.cs
+++ WebAPI/Controllers/V1/IdentityController.cs
@@ -112,6 +112,46 @@ public class IdentityController : ControllerBase
     }
 
     [HttpPost]
+    [Route("RegisterSuperUser")]
+    public async Task<IActionResult> RegisterSuperUser(RegisterModel registerModel)
+    {
+        var userExists = await _userManager.FindByNameAsync(registerModel.UserName);
+        if(userExists != null)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new Response<bool>
+            {
+                Succeeded = false,
+                Message = "User already exists!"
+            });
+        }
+
+        var user = new ApplicationUser()
+        {
+            Email = registerModel.Email,
+            SecurityStamp = Guid.NewGuid().ToString(),
+            UserName = registerModel.UserName
+        };
+
+        var result = await _userManager.CreateAsync(user, registerModel.Password);
+        if (result.Succeeded)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new Response<bool>
+            {
+                Succeeded = false,
+                Message = "User creation failed! Please check login details and try again",
+                Errors = result.Errors.Select(x => x.Description)
+            });
+        }
+
+        if (!await _roleManager.RoleExistsAsync(UserRoles.AdminOrUser))
+            await _roleManager.CreateAsync(new IdentityRole(UserRoles.AdminOrUser));
+
+        await _userManager.AddToRoleAsync(user, UserRoles.AdminOrUser);
+
+        return Ok(new Response<bool> { Succeeded = true, Message = "User created successfuly!" });
+    }
+
+    [HttpPost]
     [Route("Login")]
     public async Task<IActionResult> Login(LoginModel login)
     {
